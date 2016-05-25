a5181a76e962a83364250a495b906165deb094ea
jordimulder <jordimulder1@outlook.com>
2016-05-25 16:12:38 +0200

    Test123

---
+++ RBS/RBS/VoorraadScherm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RBS
+{
+    public partial class VoorraadScherm : Form
+    {
+        public VoorraadScherm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
