c88dd076f71fe8b1b94eee3fe9ea88325d214d40
jorritheijn <jorritheijn@gmail.com>
2016-05-25 14:20:50 +0200

    aanpassing

--- RBS/RBS/KeukenScherm.Designer.cs
+++ RBS/RBS/KeukenScherm.Designer.cs
@@ -250,7 +250,7 @@
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(83, 17);
             this.label8.TabIndex = 0;
-            this.label8.Text = "Bijwerking 2";
+            this.label8.Text = "Bijwerking 3";
             // 
             // KeukenScherm
             // 
