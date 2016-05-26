7ae00a70d45eb43a6702f19c6461224d03724d97
rene6133 <rene613@msn.com>
2016-05-26 13:29:08 +0200

    upd

--- RBS/RBS/Program.cs
+++ RBS/RBS/Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace RBS
 {
     static class Program
     {
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +19,7 @@ namespace RBS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new BarScherm());
+            Application.Run(new Afrekenen());
         }
     }
 }
