e2b9a6205be6331ef1afd995db489d8fb2121640
JDoorson <JamalDoorson@live.nl>
2016-05-26 12:37:49 +0200

    Klasse Personeel en PersoneelDAO ingevuld. Begin gemaakt aan 't PersoneelsBeheer scherm.

--- RBS/RBS/PersoneelDAO.cs
+++ RBS/RBS/PersoneelDAO.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace RBS
 {
     class PersoneelDAO
     {
+        protected SqlConnection dbConnection;
+
+        public PersoneelDAO(SqlConnection dbConnection)
+        {
+            this.dbConnection = dbConnection;
+        }
+
+        public List<Personeel> GetAll()
+        {
+            dbConnection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM personeel", dbConnection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Personeel> allePersoneel = new List<Personeel>();
+
+            while(reader.Read())
+            {
+                Personeel personeel = ReadPersoneel(reader);
+                allePersoneel.Add(personeel);
+            }
+
+            dbConnection.Close();
+
+            return allePersoneel;
+        }
+
+        private Personeel ReadPersoneel(SqlDataReader reader)
+        {
+            int id = (int)reader["id"];
+            string username = (string)reader["username"];
+            int pincode = (int)reader["pincode"];
+            string functie = (string)reader["functie"];
+
+            return new Personeel(id, username, pincode, functie);
+        }
     }
 }
---
+++ RBS/RBS/PersoneelsBeheer.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace RBS
+{
+    partial class PersoneelsBeheer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstAccounts = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lstAccounts
+            // 
+            this.lstAccounts.Location = new System.Drawing.Point(22, 161);
+            this.lstAccounts.Name = "lstAccounts";
+            this.lstAccounts.Size = new System.Drawing.Size(571, 296);
+            this.lstAccounts.TabIndex = 0;
+            this.lstAccounts.UseCompatibleStateImageBehavior = false;
+            // 
+            // PersoneelsBeheer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 559);
+            this.Controls.Add(this.lstAccounts);
+            this.Name = "PersoneelsBeheer";
+            this.Text = "PersoneelsBeheer";
+            this.Shown += new System.EventHandler(this.PersoneelsBeheer_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstAccounts;
+    }
+}
