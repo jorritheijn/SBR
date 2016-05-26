06a14a9264ef16e93f9331e842b12aeb036ea8d7
jordimulder <jordimulder1@outlook.com>
2016-05-26 14:23:04 +0200

    Manager scherm

--- RBS/RBS/BeheerScherm.Designer.cs
+++ RBS/RBS/BeheerScherm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RBS
 {
-    partial class VoorraadScherm
+    partial class BeheerScherm
     {
         /// <summary>
         /// Required designer variable.
@@ -33,15 +33,20 @@
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel8 = new System.Windows.Forms.Panel();
+            this.Prod = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.productlabel = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel8.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -49,6 +54,7 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Location = new System.Drawing.Point(83, 31);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -64,7 +70,6 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Actuele bestellingen";
             this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
             // 
             // tabPage2
             // 
@@ -89,23 +94,42 @@
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Voorraad";
             this.tabPage3.UseVisualStyleBackColor = true;
-            this.tabPage3.Click += new System.EventHandler(this.tabPage3_Click);
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Location = new System.Drawing.Point(408, 19);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(102, 469);
             this.panel1.TabIndex = 4;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label3";
+            // 
             // panel8
             // 
             this.panel8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel8.Controls.Add(this.Prod);
             this.panel8.Location = new System.Drawing.Point(0, 19);
             this.panel8.Name = "panel8";
             this.panel8.Size = new System.Drawing.Size(411, 466);
             this.panel8.TabIndex = 4;
+            // 
+            // Prod
+            // 
+            this.Prod.AutoSize = true;
+            this.Prod.Location = new System.Drawing.Point(17, 14);
+            this.Prod.Name = "Prod";
+            this.Prod.Size = new System.Drawing.Size(28, 13);
+            this.Prod.TabIndex = 0;
+            this.Prod.Text = "prod";
             // 
             // label2
             // 
@@ -126,7 +150,6 @@
             this.productlabel.Size = new System.Drawing.Size(51, 13);
             this.productlabel.TabIndex = 0;
             this.productlabel.Text = "Product";
-            this.productlabel.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // button1
             // 
@@ -163,11 +186,21 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 4);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 24);
+            this.label1.Size = new System.Drawing.Size(92, 24);
             this.label1.TabIndex = 13;
-            this.label1.Text = "Keuken";
+            this.label1.Text = "Manager";
             // 
-            // VoorraadScherm
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(506, 488);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // BeheerScherm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -178,11 +211,16 @@
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "VoorraadScherm";
-            this.Text = "VoorraadScherm";
+            this.Name = "BeheerScherm";
+            this.Text = "Beheerscherm";
+            this.Shown += new System.EventHandler(this.VoorraadScherm_Shown);
             this.tabControl1.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
             this.tabPage3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,6 +240,9 @@
         private System.Windows.Forms.Panel panel8;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Prod;
+        private System.Windows.Forms.TabPage tabPage4;
 
     }
 }
