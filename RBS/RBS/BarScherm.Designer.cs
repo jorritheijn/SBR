2b6f4ea4c0ea13e27bdd1e67cb38afe2e8ca2dda
jorritheijn <jorritheijn@gmail.com>
2016-05-31 15:23:26 +0200

    Barscherm updates

--- RBS/RBS/BarScherm.Designer.cs
+++ RBS/RBS/BarScherm.Designer.cs
@@ -29,21 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.LogOut = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.Tafel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Bestelling = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Aantal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.listView2 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -56,90 +47,39 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Baroverzicht";
             // 
-            // tabControl1
+            // LogOut
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.tabControl1.Location = new System.Drawing.Point(32, 60);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(567, 482);
-            this.tabControl1.TabIndex = 7;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tabPage1.Controls.Add(this.listView1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 26);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(559, 452);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Actuele bestellingen";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tabPage2.Controls.Add(this.listView2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 26);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(559, 452);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Afgeronde bestellingen";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.LogOut.BackColor = System.Drawing.Color.Crimson;
+            this.LogOut.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LogOut.Location = new System.Drawing.Point(524, 25);
+            this.LogOut.Name = "LogOut";
+            this.LogOut.Size = new System.Drawing.Size(75, 23);
+            this.LogOut.TabIndex = 8;
+            this.LogOut.Text = "Log uit";
+            this.LogOut.UseVisualStyleBackColor = false;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Crimson;
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(524, 25);
+            this.button1.Location = new System.Drawing.Point(216, 71);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Log uit";
-            this.button1.UseVisualStyleBackColor = false;
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // listView1
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.Tafel,
-            this.Bestelling,
-            this.Aantal});
-            this.listView1.Location = new System.Drawing.Point(-1, -1);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(559, 452);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // Tafel
-            // 
-            this.Tafel.Text = "Tafel";
-            // 
-            // Bestelling
-            // 
-            this.Bestelling.Text = "Bestelling";
-            this.Bestelling.Width = 430;
-            // 
-            // Aantal
-            // 
-            this.Aantal.Text = "Aantal";
-            // 
-            // listView2
-            // 
-            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3});
-            this.listView2.Location = new System.Drawing.Point(-1, -1);
-            this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(559, 452);
-            this.listView2.TabIndex = 1;
-            this.listView2.UseCompatibleStateImageBehavior = false;
-            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(33, 100);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(566, 391);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
             // 
             // columnHeader1
             // 
@@ -148,25 +88,25 @@
             // columnHeader2
             // 
             this.columnHeader2.Text = "Bestelling";
-            this.columnHeader2.Width = 430;
+            this.columnHeader2.Width = 445;
             // 
             // columnHeader3
             // 
             this.columnHeader3.Text = "Aantal";
+            this.columnHeader3.Width = 54;
             // 
             // BarScherm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(627, 582);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.LogOut);
             this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
             this.Name = "BarScherm";
             this.Text = "Baroverzicht";
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,15 +115,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button LogOut;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader Tafel;
-        private System.Windows.Forms.ColumnHeader Bestelling;
-        private System.Windows.Forms.ColumnHeader Aantal;
-        private System.Windows.Forms.ListView listView2;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
