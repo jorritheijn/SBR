cab2257598a95ce2e3a0315971c855d88189320e
JDoorson <JamalDoorson@live.nl>
2016-06-01 13:39:52 +0200

    test

---
+++ RBS/rbs-test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace rbs_test
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string connString = ConfigurationManager.ConnectionStrings["ReserveringenConnectionStringSQL"].ConnectionString;
+            SqlConnection dbConnection = new SqlConnection(connString);
+
+            Application.Run(new Form1());
+        }
+    }
+}
