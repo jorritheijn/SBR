c096cb5e64a6738ec2696e9d9be4f039c11072f8
jordimulder <jordi.mulder@outlook.com>
2016-06-01 13:43:22 +0200

    ProductDAO update

--- RBS/RBS/ProductDAO.cs
+++ RBS/RBS/ProductDAO.cs
@@ -9,13 +9,14 @@ namespace RBS
 {
     class ProductDAO
     {
+        
         protected SqlConnection dbConnection;
-
+       
         public ProductDAO(SqlConnection dbConnection)
         {
             this.dbConnection = dbConnection;
         }
-
+       
         public List<Product> GetAll()
         {
             dbConnection.Open();
@@ -37,16 +38,72 @@ namespace RBS
             return alleProduct;
         }
 
+         public List<Product> Dranken()
+        {
+             dbConnection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT dranken FROM product ORDER BY productId", dbConnection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Product> Dranken = new List<Product>();
+
+            while (reader.Read())
+            {
+                Product product = ReadProduct(reader);
+                Dranken.Add(product);
+            }
+
+            dbConnection.Close();
+
+            return Dranken;
+        }
+
+        
+
         private Product ReadProduct(SqlDataReader reader)
         {
-            int id = (int)reader["id"];
-            string naam = (string)reader["naam"];
-            double prijs = (double)reader["prijs"];
+            int productId = (int)reader["productId"];
+            string productNaam = (string)reader["naam"];
+            double productPrijs = (double)reader["prijs"];
             int aantalVoorraad = (int)reader["aantalVoorraad"];
 
-            return new Product(id, naam, prijs, aantalVoorraad);
+            return new Product(productId, productNaam, productPrijs, aantalVoorraad);
+        }
+        public void VoegtoeProduct(int ProductId, string productNaam, double productPrijs, int aantalVoorraad)
+        {
+            dbConnection.Open();
+            string sql = "INSERT INTO Producten (id, naam, prijs, aantal) " + "VALUES (@id, @naam, @prijs, @aantal)";
+            SqlCommand command = new SqlCommand(sql, dbConnection);
+            command.Parameters.AddWithValue("@id", ProductId);
+            command.Parameters.AddWithValue("@naam", productNaam);
+            command.Parameters.AddWithValue("@prijs", productPrijs);
+            command.Parameters.AddWithValue("@aantal", aantalVoorraad);
+            command.ExecuteNonQuery();
+
+            dbConnection.Close();
+        }
+        public void WijzigProduct(int productId, string productNaam, double productPrijs, int aantalVoorraad)
+        {
+            dbConnection.Open();
+            string sql = String.Format(
+                "UPDATE Producten " + "SET naam = @naam, prijs = @prijs, aantal= @aantal" + "WHERE Id={0}", productId);
+            SqlCommand command = new SqlCommand(sql, dbConnection);
+            command.Parameters.AddWithValue("@naam", productNaam);
+            command.Parameters.AddWithValue("@prijs", productPrijs);
+            command.Parameters.AddWithValue("@aantal", aantalVoorraad);
+            command.ExecuteNonQuery();
+        }
+        public void VerwijderProduct(int productId, string productNaam, double ProductPrijs, int aantalVoorraad)
+        {
+            dbConnection.Open();
+            string sql = String.Format("DELETE FROM Producten WHERE ID={0}", productId);
+            SqlCommand command = new SqlCommand(sql, dbConnection);
+            command.ExecuteNonQuery();
         }
     }
 }
+
+
     
 
