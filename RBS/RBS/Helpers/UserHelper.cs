91415c03db4f047da7a7b171fad17b0f1f76de81
demikei <demikei@outlook.com>
2016-06-01 21:38:16 +0200

    Gebruiker bewaren en doorsturen op basis van functie

---
+++ RBS/RBS/Helpers/UserHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RBS.Helpers
+{
+    class UserHelper
+    {
+        public static Personeel Gebruiker = null;
+
+        public static bool IsIngelogd
+        {
+            get { return Gebruiker != null; }
+        }
+
+        public static bool Inloggen(string pincode)
+        {
+            var werknemer = DataHelper.PersoneelDao.GetByPincode(pincode);
+
+            if (werknemer != null)
+            {
+                Gebruiker = werknemer;
+            }
+
+            return IsIngelogd;
+        }
+
+        public static void Uitloggen()
+        {
+            Gebruiker = null;
+        }
+    }
+}
