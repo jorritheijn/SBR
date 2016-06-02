c23b6e12953889d360d56fc7673290438b88997d
demikei <demikei@outlook.com>
2016-06-02 14:09:03 +0200

    update demi

--- RBS/RBS/VoorraadBeheer.cs
+++ RBS/RBS/VoorraadBeheer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RBS.Helpers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -39,14 +40,16 @@ namespace RBS
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void LogUitBtn_Click(object sender, EventArgs e)
         {
-            
-            InlogScherm Check = new InlogScherm();
-            Check.Show();
+            InlogScherm Inloggen = new InlogScherm();
+
+            UserHelper.Uitloggen();
+
+            Inloggen.Show();
             Hide();
         }
-        }
+    }
 
        
 
