a89fd55b7b3d7570bb39a5c25cba32d24442beea
JDoorson <JamalDoorson@live.nl>
2016-06-09 12:14:06 +0200

    Bestelknop

--- RBS/RBS/BestelScherm.Designer.cs
+++ RBS/RBS/BestelScherm.Designer.cs
@@ -45,19 +45,21 @@
             this.tabControl.Controls.Add(this.tabPageFris);
             this.tabControl.Controls.Add(this.tabPageDrank);
             this.tabControl.Controls.Add(this.tabPageKoffieThee);
-            this.tabControl.Location = new System.Drawing.Point(13, 13);
+            this.tabControl.Location = new System.Drawing.Point(17, 16);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(439, 461);
+            this.tabControl.Size = new System.Drawing.Size(585, 589);
             this.tabControl.TabIndex = 0;
             // 
             // tabPageLunch
             // 
             this.tabPageLunch.AutoScroll = true;
-            this.tabPageLunch.Location = new System.Drawing.Point(4, 22);
+            this.tabPageLunch.Location = new System.Drawing.Point(4, 25);
+            this.tabPageLunch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPageLunch.Name = "tabPageLunch";
-            this.tabPageLunch.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageLunch.Size = new System.Drawing.Size(431, 435);
+            this.tabPageLunch.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPageLunch.Size = new System.Drawing.Size(577, 560);
             this.tabPageLunch.TabIndex = 0;
             this.tabPageLunch.Text = "Lunch";
             this.tabPageLunch.UseVisualStyleBackColor = true;
@@ -65,10 +67,11 @@
             // tabPageDiner
             // 
             this.tabPageDiner.AutoScroll = true;
-            this.tabPageDiner.Location = new System.Drawing.Point(4, 22);
+            this.tabPageDiner.Location = new System.Drawing.Point(4, 25);
+            this.tabPageDiner.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPageDiner.Name = "tabPageDiner";
-            this.tabPageDiner.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageDiner.Size = new System.Drawing.Size(518, 435);
+            this.tabPageDiner.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPageDiner.Size = new System.Drawing.Size(577, 538);
             this.tabPageDiner.TabIndex = 1;
             this.tabPageDiner.Text = "Diner";
             this.tabPageDiner.UseVisualStyleBackColor = true;
@@ -76,9 +79,10 @@
             // tabPageFris
             // 
             this.tabPageFris.AutoScroll = true;
-            this.tabPageFris.Location = new System.Drawing.Point(4, 22);
+            this.tabPageFris.Location = new System.Drawing.Point(4, 25);
+            this.tabPageFris.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPageFris.Name = "tabPageFris";
-            this.tabPageFris.Size = new System.Drawing.Size(518, 435);
+            this.tabPageFris.Size = new System.Drawing.Size(577, 538);
             this.tabPageFris.TabIndex = 2;
             this.tabPageFris.Text = "Frisdranken";
             this.tabPageFris.UseVisualStyleBackColor = true;
@@ -86,9 +90,10 @@
             // tabPageDrank
             // 
             this.tabPageDrank.AutoScroll = true;
-            this.tabPageDrank.Location = new System.Drawing.Point(4, 22);
+            this.tabPageDrank.Location = new System.Drawing.Point(4, 25);
+            this.tabPageDrank.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPageDrank.Name = "tabPageDrank";
-            this.tabPageDrank.Size = new System.Drawing.Size(518, 435);
+            this.tabPageDrank.Size = new System.Drawing.Size(577, 538);
             this.tabPageDrank.TabIndex = 3;
             this.tabPageDrank.Text = "Alcoholhoudend";
             this.tabPageDrank.UseVisualStyleBackColor = true;
@@ -96,18 +101,21 @@
             // tabPageKoffieThee
             // 
             this.tabPageKoffieThee.AutoScroll = true;
-            this.tabPageKoffieThee.Location = new System.Drawing.Point(4, 22);
+            this.tabPageKoffieThee.Location = new System.Drawing.Point(4, 25);
+            this.tabPageKoffieThee.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPageKoffieThee.Name = "tabPageKoffieThee";
-            this.tabPageKoffieThee.Size = new System.Drawing.Size(518, 435);
+            this.tabPageKoffieThee.Size = new System.Drawing.Size(577, 538);
             this.tabPageKoffieThee.TabIndex = 4;
             this.tabPageKoffieThee.Text = "Warme dranken";
             this.tabPageKoffieThee.UseVisualStyleBackColor = true;
             // 
             // btnVerwerkBestelling
             // 
-            this.btnVerwerkBestelling.Location = new System.Drawing.Point(17, 624);
+            this.btnVerwerkBestelling.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerwerkBestelling.Location = new System.Drawing.Point(17, 613);
+            this.btnVerwerkBestelling.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnVerwerkBestelling.Name = "btnVerwerkBestelling";
-            this.btnVerwerkBestelling.Size = new System.Drawing.Size(163, 59);
+            this.btnVerwerkBestelling.Size = new System.Drawing.Size(585, 114);
             this.btnVerwerkBestelling.TabIndex = 2;
             this.btnVerwerkBestelling.Text = "Bestelling gereed";
             this.btnVerwerkBestelling.UseVisualStyleBackColor = true;
@@ -115,11 +123,12 @@
             // 
             // BestelScherm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 601);
+            this.ClientSize = new System.Drawing.Size(619, 740);
             this.Controls.Add(this.btnVerwerkBestelling);
             this.Controls.Add(this.tabControl);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "BestelScherm";
             this.Text = "Bestelling opnemen";
             this.tabControl.ResumeLayout(false);
