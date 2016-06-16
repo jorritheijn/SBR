1573a5f12fd40c567553cdc40aec894fabeca8a5
jordimulder <jordimulder1@outlook.com>
2016-06-16 19:04:35 +0200

    Klaar

--- RBS/RBS/VoorraadBeheer.Designer.cs
+++ RBS/RBS/VoorraadBeheer.Designer.cs
@@ -32,22 +32,22 @@
             this.button3 = new System.Windows.Forms.Button();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.btn_Diner = new System.Windows.Forms.Button();
-            this.btn_Lunch = new System.Windows.Forms.Button();
-            this.btn_Drank = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.btn_Toevoeg = new System.Windows.Forms.Button();
             this.btn_Verwijder = new System.Windows.Forms.Button();
-            this.list_Producten = new System.Windows.Forms.ListBox();
-            this.list_Aantal = new System.Windows.Forms.ListBox();
-            this.list_Prijs = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label_prijs = new System.Windows.Forms.Label();
+            this.list_Prijs = new System.Windows.Forms.ListBox();
+            this.list_Aantal = new System.Windows.Forms.ListBox();
+            this.list_Producten = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Lunch = new System.Windows.Forms.Button();
+            this.btn_Toevoeg = new System.Windows.Forms.Button();
+            this.btn_Drank = new System.Windows.Forms.Button();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage3.SuspendLayout();
-            this.tabControl1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label3
@@ -99,35 +99,67 @@
             this.btn_Diner.UseVisualStyleBackColor = true;
             this.btn_Diner.Click += new System.EventHandler(this.btn_Diner_Click);
             // 
-            // btn_Lunch
+            // btn_Verwijder
             // 
-            this.btn_Lunch.Location = new System.Drawing.Point(6, 66);
-            this.btn_Lunch.Name = "btn_Lunch";
-            this.btn_Lunch.Size = new System.Drawing.Size(69, 23);
-            this.btn_Lunch.TabIndex = 2;
-            this.btn_Lunch.Text = "Lunch";
-            this.btn_Lunch.UseVisualStyleBackColor = true;
-            this.btn_Lunch.Click += new System.EventHandler(this.btn_Lunch_Click);
+            this.btn_Verwijder.Location = new System.Drawing.Point(304, 508);
+            this.btn_Verwijder.Name = "btn_Verwijder";
+            this.btn_Verwijder.Size = new System.Drawing.Size(143, 31);
+            this.btn_Verwijder.TabIndex = 7;
+            this.btn_Verwijder.Text = "Product verwijderen";
+            this.btn_Verwijder.UseVisualStyleBackColor = true;
+            this.btn_Verwijder.Click += new System.EventHandler(this.btn_Verwijder_Click);
             // 
-            // btn_Drank
+            // panel1
             // 
-            this.btn_Drank.Location = new System.Drawing.Point(6, 124);
-            this.btn_Drank.Name = "btn_Drank";
-            this.btn_Drank.Size = new System.Drawing.Size(69, 23);
-            this.btn_Drank.TabIndex = 3;
-            this.btn_Drank.Text = "Dranken";
-            this.btn_Drank.UseVisualStyleBackColor = true;
-            this.btn_Drank.Click += new System.EventHandler(this.btn_Drank_Click);
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label_prijs);
+            this.panel1.Controls.Add(this.list_Prijs);
+            this.panel1.Controls.Add(this.list_Aantal);
+            this.panel1.Controls.Add(this.list_Producten);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(88, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(364, 484);
+            this.panel1.TabIndex = 4;
             // 
-            // label2
+            // label_prijs
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label2.Location = new System.Drawing.Point(320, 3);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Hoeveelheid";
+            this.label_prijs.AutoSize = true;
+            this.label_prijs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label_prijs.Location = new System.Drawing.Point(225, 3);
+            this.label_prijs.Name = "label_prijs";
+            this.label_prijs.Size = new System.Drawing.Size(35, 17);
+            this.label_prijs.TabIndex = 9;
+            this.label_prijs.Text = "Prijs";
+            // 
+            // list_Prijs
+            // 
+            this.list_Prijs.FormattingEnabled = true;
+            this.list_Prijs.Location = new System.Drawing.Point(228, 37);
+            this.list_Prijs.Name = "list_Prijs";
+            this.list_Prijs.Size = new System.Drawing.Size(76, 420);
+            this.list_Prijs.TabIndex = 8;
+            // 
+            // list_Aantal
+            // 
+            this.list_Aantal.FormattingEnabled = true;
+            this.list_Aantal.Location = new System.Drawing.Point(310, 37);
+            this.list_Aantal.Name = "list_Aantal";
+            this.list_Aantal.Size = new System.Drawing.Size(56, 420);
+            this.list_Aantal.TabIndex = 4;
+            this.list_Aantal.SelectedIndexChanged += new System.EventHandler(this.list_Aantal_SelectedIndexChanged);
+            // 
+            // list_Producten
+            // 
+            this.list_Producten.FormattingEnabled = true;
+            this.list_Producten.HorizontalScrollbar = true;
+            this.list_Producten.Location = new System.Drawing.Point(-2, 38);
+            this.list_Producten.Name = "list_Producten";
+            this.list_Producten.Size = new System.Drawing.Size(224, 420);
+            this.list_Producten.TabIndex = 3;
+            this.list_Producten.SelectedIndexChanged += new System.EventHandler(this.list_Producten_SelectedIndexChanged);
+            this.list_Producten.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.list_Producten_MouseDoubleClick);
             // 
             // label1
             // 
@@ -138,6 +170,46 @@
             this.label1.Size = new System.Drawing.Size(57, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Product";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(311, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Aantal";
+            // 
+            // btn_Lunch
+            // 
+            this.btn_Lunch.Location = new System.Drawing.Point(6, 66);
+            this.btn_Lunch.Name = "btn_Lunch";
+            this.btn_Lunch.Size = new System.Drawing.Size(69, 23);
+            this.btn_Lunch.TabIndex = 2;
+            this.btn_Lunch.Text = "Lunch";
+            this.btn_Lunch.UseVisualStyleBackColor = true;
+            this.btn_Lunch.Click += new System.EventHandler(this.btn_Lunch_Click);
+            // 
+            // btn_Toevoeg
+            // 
+            this.btn_Toevoeg.Location = new System.Drawing.Point(87, 508);
+            this.btn_Toevoeg.Name = "btn_Toevoeg";
+            this.btn_Toevoeg.Size = new System.Drawing.Size(143, 31);
+            this.btn_Toevoeg.TabIndex = 6;
+            this.btn_Toevoeg.Text = "Product toevoegen";
+            this.btn_Toevoeg.UseVisualStyleBackColor = true;
+            this.btn_Toevoeg.Click += new System.EventHandler(this.btn_Toevoeg_Click);
+            // 
+            // btn_Drank
+            // 
+            this.btn_Drank.Location = new System.Drawing.Point(6, 124);
+            this.btn_Drank.Name = "btn_Drank";
+            this.btn_Drank.Size = new System.Drawing.Size(69, 23);
+            this.btn_Drank.TabIndex = 3;
+            this.btn_Drank.Text = "Dranken";
+            this.btn_Drank.UseVisualStyleBackColor = true;
+            this.btn_Drank.Click += new System.EventHandler(this.btn_Drank_Click);
             // 
             // tabPage1
             // 
@@ -160,78 +232,6 @@
             this.tabControl1.TabIndex = 0;
             this.tabControl1.Click += new System.EventHandler(this.goPersoneel);
             // 
-            // btn_Toevoeg
-            // 
-            this.btn_Toevoeg.Location = new System.Drawing.Point(87, 508);
-            this.btn_Toevoeg.Name = "btn_Toevoeg";
-            this.btn_Toevoeg.Size = new System.Drawing.Size(143, 31);
-            this.btn_Toevoeg.TabIndex = 6;
-            this.btn_Toevoeg.Text = "Product toevoegen";
-            this.btn_Toevoeg.UseVisualStyleBackColor = true;
-            this.btn_Toevoeg.Click += new System.EventHandler(this.btn_Toevoeg_Click);
-            // 
-            // btn_Verwijder
-            // 
-            this.btn_Verwijder.Location = new System.Drawing.Point(304, 508);
-            this.btn_Verwijder.Name = "btn_Verwijder";
-            this.btn_Verwijder.Size = new System.Drawing.Size(143, 31);
-            this.btn_Verwijder.TabIndex = 7;
-            this.btn_Verwijder.Text = "Product verwijderen";
-            this.btn_Verwijder.UseVisualStyleBackColor = true;
-            this.btn_Verwijder.Click += new System.EventHandler(this.btn_Verwijder_Click);
-            // 
-            // list_Producten
-            // 
-            this.list_Producten.FormattingEnabled = true;
-            this.list_Producten.Location = new System.Drawing.Point(-2, 38);
-            this.list_Producten.Name = "list_Producten";
-            this.list_Producten.Size = new System.Drawing.Size(248, 420);
-            this.list_Producten.TabIndex = 3;
-            this.list_Producten.SelectedIndexChanged += new System.EventHandler(this.list_Producten_SelectedIndexChanged);
-            this.list_Producten.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.list_Producten_MouseDoubleClick);
-            // 
-            // list_Aantal
-            // 
-            this.list_Aantal.FormattingEnabled = true;
-            this.list_Aantal.Location = new System.Drawing.Point(333, 37);
-            this.list_Aantal.Name = "list_Aantal";
-            this.list_Aantal.Size = new System.Drawing.Size(76, 420);
-            this.list_Aantal.TabIndex = 4;
-            this.list_Aantal.SelectedIndexChanged += new System.EventHandler(this.list_Aantal_SelectedIndexChanged);
-            // 
-            // list_Prijs
-            // 
-            this.list_Prijs.FormattingEnabled = true;
-            this.list_Prijs.Location = new System.Drawing.Point(251, 38);
-            this.list_Prijs.Name = "list_Prijs";
-            this.list_Prijs.Size = new System.Drawing.Size(76, 420);
-            this.list_Prijs.TabIndex = 8;
-            // 
-            // panel1
-            // 
-            this.panel1.AutoScroll = true;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.label_prijs);
-            this.panel1.Controls.Add(this.list_Prijs);
-            this.panel1.Controls.Add(this.list_Aantal);
-            this.panel1.Controls.Add(this.list_Producten);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(88, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(364, 484);
-            this.panel1.TabIndex = 4;
-            // 
-            // label_prijs
-            // 
-            this.label_prijs.AutoSize = true;
-            this.label_prijs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label_prijs.Location = new System.Drawing.Point(248, 3);
-            this.label_prijs.Name = "label_prijs";
-            this.label_prijs.Size = new System.Drawing.Size(35, 17);
-            this.label_prijs.TabIndex = 9;
-            this.label_prijs.Text = "Prijs";
-            // 
             // VoorraadBeheer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -244,9 +244,9 @@
             this.Text = "VoorraadBeheer";
             this.Activated += new System.EventHandler(this.VoorraadBeheer_Activated);
             this.tabPage3.ResumeLayout(false);
-            this.tabControl1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
